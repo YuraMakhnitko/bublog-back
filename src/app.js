c43605ec6b7c9e4875de0514b2710ce48ccc8f88
YuraMakhnitko <bizoru82@gmail.com>
2023-04-12 21:50:02 +0300

    5th

--- src/app.js
+++ src/app.js
@@ -1,37 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const multer = require("multer");
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-const { storageAvatar, storageArticles } = require("./storage");
+const { storageAvatar, storageArticles } = require('./storage');
 
-const { appRouter } = require("./routes/app.router");
+const { appRouter } = require('./routes/app.router');
 
 const app = express();
 
 const uploadAvatar = multer({ storage: storageAvatar });
 const uploadArticleImage = multer({ storage: storageArticles });
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
 app.use(appRouter);
 
 // routes for upload users avatars and articles images
-app.use("/src/uploads", express.static("src/uploads"));
+app.use('/src/uploads', express.static('src/uploads'));
 
-app.post("/upload/avatars", uploadAvatar.single("image"), (req, res) => {
+app.post('/upload/avatars', uploadAvatar.single('image'), (req, res) => {
   console.log(req.file);
   res.json({
     url: `/uploads/avatars/${req.file.originalname}`,
   });
 });
 
-app.post("/upload/articles", uploadArticleImage.single("image"), (req, res) => {
+app.post('/upload/articles', uploadArticleImage.single('image'), (req, res) => {
   console.log(req.file);
   res.json({
     url: `/uploads/articles/${req.file.originalname}`,
