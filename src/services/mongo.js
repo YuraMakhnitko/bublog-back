874689825053697173aca5a580adeda2cc5d23eb
YuraMakhnitko <bizoru82@gmail.com>
2023-04-16 19:16:12 +0300

    1

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const MONGO_URL =
-  'mongodb+srv://YuraM:cosuBkLBv9O6Tsq4@node.qcss5fg.mongodb.net/byblog?retryWrites=true&w=majority';
+// const MONGO_URL =
+//   'mongodb+srv://YuraM:cosuBkLBv9O6Tsq4@node.qcss5fg.mongodb.net/byblog?retryWrites=true&w=majority';
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
@@ -16,8 +16,8 @@ mongoose.connection.on('error', (error) => {
 });
 
 async function mongoConnect() {
-  // await mongoose.connect(process.env.MONGODB_URI);
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGODB_URI);
+  // await mongoose.connect(MONGO_URL);
 }
 
 async function mongoDisconect() {
